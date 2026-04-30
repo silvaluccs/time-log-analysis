da4ed628b8c70662b409f9941b64d361c79adb8d
Lucas Oliveira <lucasoliveiraecomp@gmail.com>
2026-04-29 21:44:15 -0300

    chore: change file output name

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
 import src.app as app
 
 if __name__ == "__main__":
-    app.run_analysis_and_save_results("data.json", "output.json")
+    app.run_analysis_and_save_results("data.json", "result.json")
